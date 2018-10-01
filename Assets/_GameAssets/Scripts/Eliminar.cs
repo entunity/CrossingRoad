a9d4ccc6fa6273eb95a1eb8e8a107974582d48c0
enrique <enrique.tunity@gmail.com>
2018-10-01 13:41:57 +0200

    Initial commit

---
+++ Assets/_GameAssets/Scripts/Eliminar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eliminar : MonoBehaviour {
+
+
+    Vector3 posini;
+    [SerializeField] Vector3 posfinal;
+    [SerializeField] float velocidad;
+
+    private AudioSource audio;
+
+    // Use this for initialization
+    void Start () {
+        posini = this.transform.position;
+        audio = this.GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = Vector3.Lerp(posini, posfinal, (Mathf.Sin(velocidad * Time.time) + 1.0f) / 2.0f);
+        //this.transform.Translate(posfinal);
+	}
+    /*
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            Debug.Log("Objeto conseguido");
+            Destroy(this.gameObject);
+        }
+    }*/
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Objeto conseguido");
+            Destroy(this.gameObject);
+            audio.Play();
+        }
+    }
+}
---
+++ Assets/_GameAssets/Scripts/Mover.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+
+    
+    [SerializeField] float fuerza;
+    [SerializeField] float tiempoentresaltos;
+    [SerializeField] Transform posPies;
+
+    private Rigidbody rb;
+    private float tiempoactual;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+
+        if (Input.GetKeyDown("space")&&Time.time>tiempoactual) {
+
+            //comprueba q tiene alrededor de ese pto
+            Collider[] cois=Physics.OverlapSphere(posPies.position, 0.1f, LayerMask.NameToLayer("Terreno"));
+            for (int i = 0; i < cois.Length; i++) {
+                Debug.Log(cois[i].name);
+            }
+
+
+            rb.AddForce(new Vector3(0f,1f,1f) *fuerza);
+            tiempoactual = Time.time + tiempoentresaltos;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        else if (Input.GetKeyDown("d") && Time.time > tiempoactual)
+        {
+            rb.AddForce(new Vector3(1f, 1f, 0f) * fuerza);
+            tiempoactual = Time.time + tiempoentresaltos;
+            //transform.eulerAngles = new Vector3(0, 90, 0);
+        }
+        else if (Input.GetKeyDown("a") && Time.time > tiempoactual)
+        {
+            rb.AddForce(new Vector3(-1f, 1f, 0f) * fuerza);
+            tiempoactual = Time.time + tiempoentresaltos;
+            //transform.eulerAngles = new Vector3(0, -90, 0);
+        }
+
+
+    }
+}
